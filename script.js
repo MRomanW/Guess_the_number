74d17ecf830957b352f13e227d0fcfc2c9b65834
MRomanW <romanmelnyk21@gmail.com>
2023-09-10 23:01:37 +0300

    javascript code

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.score').textContent = score;
+
+document.querySelector('.question').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+
+  const guessingNumber = Number(document.querySelector('.number-input').value)
+  
+  if (!guessingNumber ) {
+    document.querySelector('.guess-message').textContent = 'Input some number!';
+  } else if (guessingNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = "That's right!";
+  } else if (guessingNumber > secretNumber) {
+    if (score > 1 ) {
+      document.querySelector('.guess-message').textContent = "Too much!";
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = "Game Over!";
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guessingNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = "Not enough!";
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = "Game Over!";
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+});
